a61561eb60c8a489e76c86ab149a71ffd0e9e792
Micaela <mikiluzcabrera1@gmail.com>
2022-11-23 17:36:37 -0300

    cambios

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,1 +1,9 @@
-git
+addEventListener('DOMContentLoaded'), () => {
+    const btn-menu = document.querySelector('.btn-menu')
+    if (btn-menu){
+        btn-menu.addEventListener('click',() => {
+        const NavBar_List = document.querySelector('.NavBar_List')
+        NavBar_List.classList.toggle('show')
+        })
+    }
+}
